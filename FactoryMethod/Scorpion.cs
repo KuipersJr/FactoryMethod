c906cf2b466902ed34034ec6ec2546c0573fc1dd
vanderlei.junior <vanderlei.junior@bgmrodotec.com.br>
2019-04-22 13:17:40 -0300

    FactoryMethod

---
+++ FactoryMethod/Scorpion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Scoporion : IPersonagem
+    {
+        public void Escolhido()
+        {
+            Console.WriteLine("Scoporion");
+        }
+    }
+}
